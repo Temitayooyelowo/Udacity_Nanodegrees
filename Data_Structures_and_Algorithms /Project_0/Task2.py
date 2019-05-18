980653e7f2f418b22c21abf1fbe8a476a62bc589
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2019-05-17 23:11:44 -0400

    Add Task2.py

--- Data_Structures_and_Algorithms /Project_0/Task2.py
+++ Data_Structures_and_Algorithms /Project_0/Task2.py
@@ -21,4 +21,34 @@ September 2016.".
 """
 
 
-print("{} spent the longest time, {} seconds, on the phone during September 2016.")
+def solution():
+
+    tel_dict = {}
+    max_num = None
+
+    for call in calls:
+        num_seconds = int(call[3])
+
+        if max_num is None:
+            max_num = call[0]
+
+        if(call[0] in tel_dict):
+            tel_dict[call[0]] += num_seconds
+        else:
+            tel_dict[call[0]] = num_seconds
+
+        if(call[1] in tel_dict):
+            tel_dict[call[1]] += num_seconds
+        else:
+            tel_dict[call[1]] = num_seconds
+
+        if(tel_dict[call[0]] > tel_dict[max_num]):
+            max_num = call[0]
+        if(tel_dict[call[1]] > tel_dict[max_num]):
+            max_num = call[1]
+
+    print(
+        f"{max_num} spent the longest time, {tel_dict[max_num]} seconds, on the phone during September 2016.")
+
+
+solution()
